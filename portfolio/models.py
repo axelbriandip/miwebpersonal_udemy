345a897983b065384166c1b7706d5828d89cc9e5
Axel Brian Dip <axelbriandip.rg@gmail.com>
2025-03-19 19:46:19 -0300

    image functionality ok

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200, verbose_name="titulo")
+    description = models.TextField(verbose_name="descripción")
+    image = models.ImageField(verbose_name="imágen", upload_to="projects")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="últ. fecha de edición")
 
     class Meta:
         verbose_name = "proyecto"
