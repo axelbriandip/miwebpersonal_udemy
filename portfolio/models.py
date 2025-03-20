b8913fb5afea4b9997ced7f18d28cd33a16b4f62
Axel Brian Dip <axelbriandip.rg@gmail.com>
2025-03-19 23:18:34 -0300

    most info add

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="titulo")
     description = models.TextField(verbose_name="descripción")
     image = models.ImageField(verbose_name="imágen", upload_to="projects")
+    link = models.URLField(verbose_name="link", null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name="fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="últ. fecha de edición")
 
